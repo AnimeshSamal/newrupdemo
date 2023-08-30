dd9213864d3010bb6cc6da4a1f47a5fa689e00ad
Animesh <animeshsamal1995@gmail.com>
2023-08-31 02:44:34 +0530

    second

--- newrup/settings.py
+++ newrup/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zj11(2*qbi8ybu3$)fl3%0sb5r95)#z(d66!@bywk*04ju91r2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
